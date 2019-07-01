aaae5fbf6590495df216153435c89218a03c63f4
ankur <ankurg201@gmail.com>
2019-07-01 17:52:04 +0530

    adding jdbc code

--- jdbcapp/src/main/java/com/techchefs/jdbcapp/MyFirstJdbcPrograme.java
+++ jdbcapp/src/main/java/com/techchefs/jdbcapp/MyFirstJdbcPrograme.java
@@ -1,5 +1,60 @@
 package com.techchefs.jdbcapp;
 
-public class MyFirstJdbcPrograme {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.mysql.jdbc.Driver;
+
+public class MyFirstJdbcPrograme {
+	private static final Logger LOGGER = Logger.getLogger("MyFirstJdbcPrograme");
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			// 1. load the driver.
+			Driver driver = new Driver();
+			DriverManager.deregisterDriver(driver);
+
+			// 2. get the db connection via driver
+			String dbUrl = "jdbc:mysql://localhost:3306/techchefs_db?user=root&password=admin";
+			con = DriverManager.getConnection(dbUrl);
+
+			// 3. issue sql query via connection
+			String query = "select * from employee_info";
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				LOGGER.log(Level.INFO, "ID-> " + rs.getInt("id"));
+				LOGGER.log(Level.INFO, "NAME-> " + rs.getString("name"));
+				LOGGER.log(Level.INFO, "AGE-> " + rs.getInt("age"));
+				LOGGER.log(Level.INFO, "GENDER-> " + rs.getString("gender"));
+				LOGGER.log(Level.INFO, "====================================");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// 5. close all connection
+				if (con != null) {
+					con.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
