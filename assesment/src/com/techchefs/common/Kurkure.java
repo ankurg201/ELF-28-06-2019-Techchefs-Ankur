82aeef7ca6cffd0ad8d64ec9b3856244eb1f36ed
ankur-gupta001 <ankur.gupta@techchefs.in>
2019-07-03 23:11:36 +0530

    some assignemnt file added and deleted

--- assesment/src/com/techchefs/common/Kurkure.java
+++
@@ -1,6 +0,0 @@
-package com.techchefs.common;
-
-public class Kurkure {
-	
-	
-}
--- assesment/src/com/techchefs/common/Test2.java
+++
@@ -1,8 +0,0 @@
-package com.techchefs.common;
-
-public class Test2 {
-	
-	public static void main(String[] args) {
-		
-	}
-}
