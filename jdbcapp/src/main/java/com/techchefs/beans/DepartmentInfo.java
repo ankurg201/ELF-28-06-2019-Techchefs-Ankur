837568dab599627e7cb6b2abcbe704e86970aa74
ankur <ankurg201@gmail.com>
2019-07-03 18:41:32 +0530

    beans class

---
+++ jdbcapp/src/main/java/com/techchefs/beans/DepartmentInfo.java
@@ -0,0 +1,8 @@
+package com.techchefs.beans;
+
+public class DepartmentInfo {
+
+	private Integer deptId;
+	
+	private String deptName;
+}
---
+++ jdbcapp/src/main/java/com/techchefs/beans/TrainingInfo.java
@@ -0,0 +1,5 @@
+package com.techchefs.beans;
+
+public class TrainingInfo {
+
+}
